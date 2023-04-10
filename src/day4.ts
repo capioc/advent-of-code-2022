75496f1e38e93f68d8ba9a3ba19f77b9442279c0
angelos <angelos@founda.com>
2023-04-10 09:34:13 +0200

    feat: add day 4

--- src/day4.ts
+++ src/day4.ts
@@ -1,3 +1,44 @@
-export const day4 = (): number => {
-  return 4;
+const processInput = (input: string[]) => {
+  const arr = input.map((pair) => {
+    const sectionRanges = pair.split(',');
+    return sectionRanges.map((range) => {
+      const r = range.split('-');
+      return [+r[0],+r[1]];
+    });
+  });
+  return arr;
+};
+
+const solveA = (input:any): number => {
+  let fullyContained = 0;
+  for (const pair of input) {
+    if (pair[0][0] <= pair[1][0] && pair[0][1] >= pair[1][1]) {
+      fullyContained++;
+    } else if (pair[0][0] >= pair[1][0] && pair[0][1] <= pair[1][1]) {
+      fullyContained++;
+    }
+  }
+  return fullyContained;
+};
+
+const solveB = (input: any): number => {
+  let overlapping = 0;
+  for (const pair of input) {
+    if (pair[0][0] >= pair[1][0] && pair[0][0] <= pair[1][1]) {
+      overlapping++;
+    } else if (pair[0][1] >= pair[1][0] && pair[0][1] <= pair[1][1]) {
+      overlapping++;
+    } else if (pair[1][0] >= pair[0][0] && pair[1][0] <= pair[0][1]) {
+      overlapping++;
+    } else if (pair[1][1] >= pair[0][0] && pair[1][1] <= pair[0][1]) {
+      overlapping++;
+    }
+  }
+  return overlapping;
 }
+export const day4 = (input: any): void => {
+  const processedInput = processInput(input);
+  console.log('\n-------- DAY 4 ---------')
+  console.log('Part A:', solveA(processedInput));
+  console.log('Part B:', solveB(processedInput));
+};
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
 import { day4 } from './day4';
+import { readFileSync } from 'fs';
 
-console.log('hello', day4());
+const readInput = (dayNumber: number) => {
+  try {
+    return readFileSync(`./inputs/day${dayNumber}.txt`).toString().trim().split('\n');
+  } catch (error) {
+    console.error('Error reading input', error);
+  }
+}
+
+const executeDay = (day: number) => {
+  const input = readInput(day);
+  const result = day4(input);
+  return result;
+}
+
+console.log('hello', executeDay(4));
